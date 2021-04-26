edd9ec9f6cd26408c2a2effa992db09583071a2b
Sim1810 <kaursim1810@gmail.com>
2021-04-26 15:49:11 -0400

    ships in random locations

--- app.js
+++ app.js
@@ -22,10 +22,10 @@ document.addEventListener('DOMContentLoaded' , () =>{
 
 
    //create board
-   function createBoard(){
+   function createBoard(grid, squares){
       for ( let i = 0; i < width * width; i++){
         const square =   document.createElement('div')
-        userSquares.dataset.id = i
+        square.dataset.id = i
         grid.appendChild(square)
         squares.push(square)
 
@@ -39,7 +39,8 @@ document.addEventListener('DOMContentLoaded' , () =>{
        {
            name: 'destroyer' ,
            directions: [
-               [0,1],[0,width]
+               [0,1],
+               [0,width]
            ]
        },
        {
@@ -59,14 +60,14 @@ document.addEventListener('DOMContentLoaded' , () =>{
        {
         name: 'battleship',
         directions: [
-            [0,1,2],
+            [0,1,2,3],
             [0,width,width*2,width*3]
         ]
     },
     {
         name: 'carrier',
         directions: [
-            [0,1,2],
+            [0,1,2,3,4],
             [0,width,width*2,width*3, width*4]
         ]
     },
@@ -74,14 +75,14 @@ document.addEventListener('DOMContentLoaded' , () =>{
    ]
 
 //draw the computers ships in random locations
-function generateShips(){
+function generate(ship){
     let randomDirection = Math.floor(Math.random() * ship.directions.length)
     let current = ship.directions[randomDirection]
     if(randomDirection ===0) direction = 1
     if(randomDirection === 1) direction = 10
     let randomStart = Math.abs(Math.floor(Math.random() * computerSquares.length - (ship.directions[0].length * direction)))
 
-    const isTaken = current.some(index => computerSquares[randomStart + index].classList.contai)
+    const isTaken = current.some(index => computerSquares[randomStart + index].classList.contains('taken'))
     const isAtRightEdge = current.some(index => (randomStart + index) % width === -1)
     const isAtLeftEdge = current.some(index => (randomStart + index) % width === 0)
    
